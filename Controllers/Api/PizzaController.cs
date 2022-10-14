161da7945a1dc451e53b47dcf1210dca9f1e2c82
luca-gif <luca.89m@gmail.com>
2022-10-14 15:01:23 +0200

    Creato controller api e fatta chiamata in get

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,22 @@
+﻿using la_mia_pizzeria_static.Context;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        readonly Restaurant _db = new Restaurant();
+
+        public IActionResult Get()
+        {
+            List<Pizza> pizzas = _db.ListaPizze.ToList();
+
+            return Ok(pizzas);
+        }
+    }
+}
